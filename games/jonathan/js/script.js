ed84be07bffce1d23db57ee4bddd17eb972f00ac
DrMaddMam <Luth.passhacker@gmail.com>
2025-08-26 19:57:44 -0400

    updated jonathan game

--- games/jonathan/js/script.js
+++ games/jonathan/js/script.js
@@ -3,17 +3,28 @@ function sleep(ms) {
 }
 
 let responsesI = 0;
+let done = false;
+let dblClickCheck = false;
 
 async function saySmt() {
-  let responses = ["Hello, I'm Jonathan!", "Hi again!", "Hi?", "Can you stop?", "Go away!", "Stop poking me.", "Leave me alone.", "...", ""];
-  document.getElementById("middleText").textContent = responses[responsesI];
-  if (responsesI < responses.length) {
-    responsesI++;
+    if (!done) {
+    let responses = ["Hello, I'm Jonathan!", "Hi again!", "Hi?", "Can you stop?", "Go away!", "Stop poking me.", "Leave me alone.", "...", ""];
+    document.getElementById("middleText").textContent = responses[responsesI];
+    if (responsesI < responses.length) {
+      responsesI++;
+    }
+    else if (responsesI = responses.length) {
+      document.getElementById("speakBtn").hidden = true;
+      responsesI = 0;
+      await sleep(10000);
+      document.getElementById("speakBtn").hidden = false;
+      done = true;
+      saySmt();
+    }
   }
-  else if (responsesI = responses.length) {
-    document.getElementById("speakBtn").hidden = true;
-    responsesI = 0;
-    await sleep(10000);
-    document.getElementById("speakBtn").hidden = false;
+  else if (!dblClickCheck) {
+    document.getElementById("middleText").textContent = "Goodbye";
+    await sleep(2000);
+    window.location.href = "../";
   }
 }
